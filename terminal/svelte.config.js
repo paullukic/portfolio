6df5d1c5dcebf375d2334b4c31b8502f085def3c
Paul <paul@teodesk.com>
2022-09-04 21:23:12 +0200

    changed from public to build folder

--- terminal/svelte.config.js
+++ terminal/svelte.config.js
@@ -15,8 +15,8 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'public',
-			assets: 'public',
+			pages: 'build',
+			assets: 'build',
 			fallback: null,
 			precompress: false
 		  }),
